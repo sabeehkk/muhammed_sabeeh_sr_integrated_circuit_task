dd89369085b963fdc5a6ab70c78bcb0c1e78e085
sabeehkk <sabeehkadungalloor573@gmail.com>
2024-08-13 17:23:14 +0530

    second commit

---
+++ src/components/pages/container.jsx
@@ -0,0 +1,16 @@
+import React from 'react'
+
+const Container = () => {
+  return (
+   <>
+    <div className="bg-[#fcd469] min-h-screen flex p-4 sm:p-8 md:p-12 lg:p-14 xl:p-16">
+      <div className="w-full max-w-4xl mx-auto p-4">
+
+
+      </div>
+  </div>
+   </>
+  )
+}
+
+export default Container
---
+++ src/components/weather_widjet.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+const Weather_widjet = () => {
+  return (
+    <div>
+      
+    </div>
+  )
+}
+
+export default Weather_widjet
