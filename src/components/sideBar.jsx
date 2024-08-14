54b6a0a4d83165d07ad6ecc460119a1a7c53df61
sabeehkk <sabeehkadungalloor573@gmail.com>
2024-08-14 23:17:55 +0530

    updated hamsmburger

--- src/components/sideBar.jsx
+++ src/components/sideBar.jsx
@@ -2,17 +2,20 @@
 const Sidebar = ({ isOpen, toggleSidebar }) => {
   return (
     <div
-      className={`fixed top-0 right-0 h-full w-64 bg-gray-800 text-white p-5 shadow-lg transform lg:hidden ${
-        isOpen ? "translate-x-0" : "translate-x-full"
-      } transition-transform duration-300 ease-in-out`}
+      className={`fixed top-0 right-0 h-full w-64 bg-gray-800 text-white p-5 shadow-lg transform lg:hidden ${isOpen ? "translate-x-0" : "translate-x-full"} transition-transform duration-300 ease-in-out`}
     >
+      <div className=" flex justify-between items-center">
+      <h1 className="text-2xl  font-bold  tracking-widest text-[#94a8d6]">
+      FRANK<span className="text-white">LARC</span>
+      </h1>
       <button
         className="text-2xl mb-4 focus:outline-none"
         onClick={toggleSidebar}
-      >
-        &times;
+        >
+        x
       </button>
-      <ul className="space-y-4">
+        </div>
+      <ul className="space-y-8">
         <li>
           <a href="#pricing">Pages</a>
         </li>
